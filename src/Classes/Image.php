cc595f265e2fab839cde4d81988a1a4249faede6
Jorge Emiliano Maldonado <jorgeemilianom@gmail.com>
2024-01-08 16:56:11 -0300

    Refactor Class Image

--- src/Classes/Image.php
+++ src/Classes/Image.php
@@ -13,32 +13,23 @@ class Image extends CoreAbstract
     /**
      * Method for upload image 
      */
-    public static function Upload(string $keyFile, string $pathUpload, string &$URL_IMG): bool
+    public static function Upload(string $keyFile, string $pathUpload, string $nameAssigned, string &$URL_IMG): bool
     {
         try {
-            $business = $_SESSION['Business']->info['business'];
             $fileIMG = $_FILES[$keyFile];
-            $file_type = $fileIMG['type'];
-            $file_type = explode('/', $file_type);
-            $file_type = $file_type[1];
-            // Generar un nuevo nombre de archivo único
+
+            // Extension file
             $imageFileType = strtolower(pathinfo($fileIMG["name"], PATHINFO_EXTENSION));
-            $newFileName = uniqid($business."_", true) . '.' . $imageFileType;
+
+            $allowedTypes = ['gif', 'png', 'jpg', 'jpeg', 'webp', 'bmp'];
+            if (!in_array($imageFileType, $allowedTypes)) {
+                return false;
+            }
+            
+            $newFileName = $nameAssigned . '.' . $imageFileType;
             $target_dir = "../upload/$pathUpload/"; // Directorio donde se guardarán los archivos
             $target_file = $target_dir . $newFileName;
-            # Creamos un recurso de imagen 
-            $allowedTypes = ['gif', 'png', 'jpg', 'jpeg', 'webp', 'bmp'];
-            if (!in_array($file_type, $allowedTypes)) {
-                return false;
-            }
-
-            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-            // Verificar el tamaño del archivo
-            if ($fileIMG["size"] > 500000) { // 500 KB como límite
-                return false;
-            }
-
+            
             if (move_uploaded_file($fileIMG["tmp_name"], $target_file)) {
                 $URL_IMG = PATH_UPLOAD . "$pathUpload/$newFileName";
                 return true;
