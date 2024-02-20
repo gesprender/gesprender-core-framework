40ae01d208e4387193a4e6fd7ceb8045735a7b4e
Jorge Emiliano Maldonado <jorgeemilianom@gmail.com>
2024-02-20 18:02:10 -0300

    Fix logger. Right now we used Nginx

--- src/Classes/Logger.php
+++ src/Classes/Logger.php
@@ -20,20 +20,6 @@ class Logger
     }
     public static function registerLog(string $errorLog): void
     {
-        try {
-            $dirPath = __DIR__ . '/../../Logs';
-            if (!is_dir($dirPath)) {
-                mkdir($dirPath, 0777, true);
-            }
-
-            $pathLog = $dirPath . '/errors.log';
-            $file_errors = fopen($pathLog, 'a');
-            if ($file_errors !== false) {
-                fwrite($file_errors, $errorLog);
-                fclose($file_errors);
-            }
-        } catch (\Throwable $th) {
-            error_log($th->getMessage());
-        }
+        error_log($errorLog);
     }
 }
