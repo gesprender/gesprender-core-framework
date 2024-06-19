defb7603d8f3020353c0a97be50cc403fd968708
Jorge Emiliano Maldonado <jmaldonado@itpatagonia.com>
2024-06-19 11:58:47 -0300

    <feature> update Context and remove DB class

--- src/Classes/Context.php
+++ src/Classes/Context.php
@@ -1,8 +1,12 @@
 <?php
 namespace Core\Classes;
+use Backoffice\Modules\Business\Domain\Business;
+use Backoffice\Modules\User\Domain\User;
 
 class Context {
     public $Entities = [];
+    public User $User;
+    public Business $Business;
 
     public static function getContext(): self
     {
