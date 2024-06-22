abdd817961762b0baa1d4c028c4c3f50e8808668
Jorge Emiliano Maldonado <jmaldonado@itpatagonia.com>
2024-06-22 14:44:13 -0300

    <fix> run task default

--- run-project.js
+++ run-project.js
@@ -18,6 +18,10 @@ const filesToCopy = [
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'Dockerfile.react'), dst: path.join(__dirname, 'Dockerfile.react') },
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'docker-compose.yml'), dst: path.join(__dirname, 'docker-compose.yml') },
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', '.dockerignore'), dst: path.join(__dirname, '.dockerignore') },
+    { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'nginx.conf'), dst: path.join(`${__dirname}/Docker`, 'nginx.conf') }
+];
+
+const filesToCopyInDockerFolder = [
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'nginx.conf'), dst: path.join(__dirname, 'nginx.conf') }
 ];
 
@@ -31,3 +35,4 @@ const copyFile = (src, dst) => {
 }
 
 filesToCopy.forEach(file => copyFile(file.src, file.dst));
+filesToCopyInDockerFolder.forEach(file => copyFile(file.src, file.dst));
