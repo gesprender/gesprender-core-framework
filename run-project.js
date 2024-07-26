5024766b2636a5e6c4caea3cca0c503f42d6c4af
Jorge Emiliano Maldonado <jmaldonado@itpatagonia.com>
2024-07-26 17:21:16 -0300

    <feature> fix base docker

--- run-project.js
+++ run-project.js
@@ -15,6 +15,7 @@ if (!fs.existsSync(backofficeDir)) {
 
 const filesToCopy = [
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'Dockerfile'), dst: path.join(__dirname, 'Dockerfile') },
+    { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'Dockerfile.react'), dst: path.join(__dirname, 'Dockerfile.react') },
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', 'docker-compose.yml'), dst: path.join(__dirname, 'docker-compose.yml') },
     { src: path.join(__dirname, 'Backoffice/configuration/Docker', '.dockerignore'), dst: path.join(__dirname, '.dockerignore') },
 ];
