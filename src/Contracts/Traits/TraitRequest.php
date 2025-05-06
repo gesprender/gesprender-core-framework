446e6573dcc217bfbc6660500d3ada6f5483930a
Jorge E. Maldonado <ext.jmaldonado@aper.com>
2025-05-06 14:46:26 -0300

    <feature> add phpUnit

--- src/Contracts/Traits/TraitRequest.php
+++ src/Contracts/Traits/TraitRequest.php
@@ -3,14 +3,18 @@ declare(strict_types=1);
 
 namespace Core\Contracts\Traits;
 
-use Core\Services\Validations;
+use Core\Classes\Context;
 use Core\Services\Request;
-use InvalidArgumentException;
 
 trait TraitRequest
 {
     public function getValue(string $key): mixed
     {
         return Request::getValue($key);
+    }
+    
+    public function getContext(): Context
+    {
+        return Context::getContext();
     }
 }
